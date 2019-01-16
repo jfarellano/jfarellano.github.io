1d5e27c9937b3f3fbdef7051e43a4317079d09eb
jfarellano <jfarellano@uninorte.edu.co>
2019-01-15 23:01:40 -0500

    Lots of changes

--- main.js
+++ main.js
@@ -16,8 +16,50 @@ $(document).ready(function () {
             $('.navbar .logo').fadeIn(100);
         }
     })
+    $("#who").click(function(){
+        $.scrollify.move("#2");
+    })
+    $("#home").click(function(){
+        $.scrollify.move("#1");
+    })
+    $("#work").click(function(){
+        $.scrollify.move("#3");
+    })
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip();   
+    });
+
     $.scrollify({
         section : "section",
-        sectionName : ".home, .who, .work"
+        sectionName : "1, 2, 3",
+        touchScroll:true,
+        scrollbars: true
     });
+
+
+
+    var $item = $('.carousel-item'); 
+    var $wHeight = $(window).height();
+    $item.eq(0).addClass('active');
+    $item.height($wHeight); 
+    $item.addClass('full-screen');
+
+    $('.carousel .bg').each(function() {
+        var $src = $(this).attr('src');
+        var $color = $(this).attr('data-color');
+        $(this).parent().css({
+            'background-image' : 'url(' + $src + ')',
+            'background-color' : $color
+        });
+        $(this).remove();
+    });
+
+    $(window).on('resize', function (){
+        $wHeight = $(window).height();
+        $item.height($wHeight);
+    });
+
+    $('.carousel').carousel({
+        keyboard: true
+    })
 });
